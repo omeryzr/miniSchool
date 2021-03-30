d667b6de9ca47f4b4ff01e7e30b7460f3d9ec251
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-03-30 09:28:31 -0700

    Created model of Student

--- Readme.md
+++ Readme.md
@@ -1,4 +1,20 @@
-Java, Spring Boot, RestAPI, UnitTest, Spring Security, PostgreSQL, NoSQL, Maven~Tomcat
-JavaScript, ReactJS, Redux
-AWS, Docker, Kubernetes
- 
+Back-end / Java, Spring Boot, RestAPI, UnitTest, Spring Security, PostgreSQL, NoSQL, Maven~Tomcat
+Front-End / JavaScript, ReactJS, Redux
+Deploying / AWS, Docker, Kubernetes
+
+
+Review to Models
+
+    Student - Instructor - Lesson 
+    
+    Student
+    
+    1 - With Standard technique 
+        Description for Annotations 
+            @Entity 
+            @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+            @Column('student_id')
+    Click for review of student model codes -> 
+     
+    2 - Create with Lombok  
--- src/main/java/com/miniSchool/MiniSchool/models/Student.java
+++ src/main/java/com/miniSchool/MiniSchool/models/Student.java
@@ -1,4 +1,57 @@
 package com.miniSchool.MiniSchool.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long studentId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "student_grade")
+    private int studentGrade;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getStudentGrade() {
+        return studentGrade;
+    }
+
+    public void setStudentGrade(int studentGrade) {
+        this.studentGrade = studentGrade;
+    }
+
+    public Student() {
+    }
+
 }
