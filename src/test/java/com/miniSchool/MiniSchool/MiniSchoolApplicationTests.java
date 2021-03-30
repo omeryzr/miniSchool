268bef9f0f31c2cf1e749b5b8aaaeaa6bb00938e
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-03-30 08:55:50 -0700

    project files are creating

---
+++ src/test/java/com/miniSchool/MiniSchool/MiniSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniSchool.MiniSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
