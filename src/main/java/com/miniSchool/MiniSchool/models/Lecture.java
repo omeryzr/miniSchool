7fcbce9755be77a321cc37a0e91befb45f147c7c
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-03-30 10:04:15 -0700

    created model of Lecture

--- src/main/java/com/miniSchool/MiniSchool/models/Lecture.java
+++ src/main/java/com/miniSchool/MiniSchool/models/Lecture.java
@@ -1,4 +1,25 @@
 package com.miniSchool.MiniSchool.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Lecture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lecture_id")
+    private Long lectureId;
+
+    @Column(name = "lecture_name")
+    private String lectureName;
+
+    @Column(name = "lecture_grade")
+    private int grade;
 }
